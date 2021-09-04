c61d14c6ee29273656510a7adf5484fc63ef71ae
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-09-04 18:12:12 +1200

    window opens

--- Direct3DEngine/WinMain.cpp
+++ Direct3DEngine/WinMain.cpp
@@ -1,11 +1,40 @@
 #include <Windows.h>
 
-int CALLBACK WinMain(
-	HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
-	int nCmdShow)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	const auto pClassName = L"MainWindow";
+
+	//register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = nullptr;
+	wc.hIconSm = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	RegisterClassEx(&wc);
+
+	//create window instance
+	HWND hWnd = CreateWindowEx(
+		0,
+		pClassName,
+		L"MainWindow",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		0, 0,
+		1366, 720,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+
 	while (true);
 	return 0;
 }
